3e360240fb5ef9336617ecd59c78c29418b3daa8
YunJoSubba <nikeshphago25@gmail.com>
2021-07-30 17:53:50 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-d76v24
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d76v24)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./style.css";
+
+class App extends Component{
+  render(){
+    return(
+    <div>
+      <h1>This is heading</h1>
+      <p>This is a paragraph</p>
+    </div>
+    );
+  }
+}
+export default App;
